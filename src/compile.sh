51ed24225bb9c90229ba08293cd58b499cfd573b
Nima Moshiri <a1moshir@calab-fe.cluster>
2018-08-10 16:39:52 -0700

    Fixed executable script

--- src/compile.sh
+++ src/compile.sh
@@ -3,11 +3,12 @@ EXE='DSPS-HIV'
 rm -f $EXE
 javac -cp ".:commons-math3-3.6.1.jar" ./individualBasedModel/DiscreteSpatialPhyloSimulator.java
 echo '#!/usr/bin/env bash' > $EXE
-echo 'XML="$PATH/demeSIR_acuteHigh_ART1.0_mig_trt.xml"' >> $EXE
-echo 'if [ "$#" -ne 0 ] ; then XML="$(readlink -m "$1")" ; fi' >> $EXE
 echo 'JAVA=$(which java)' >> $EXE
-echo 'PATH="$( cd "$(dirname "$0")" ; pwd -P )"' >> $EXE
-echo 'cd $PATH' >> $EXE
+echo 'ORIG=$(pwd)' >> $EXE
+echo 'P=$(cd $(dirname $0) ; pwd -P ; cd $ORIG)' >> $EXE
+echo 'XML="$P/demeSIR_acuteHigh_ART1.0_mig_trt.xml"' >> $EXE
+echo 'if [ "$#" -ne 0 ] ; then XML="$(readlink -m "$1")" ; fi' >> $EXE
+echo 'cd $P' >> $EXE
 echo '$JAVA -cp ".:commons-math3-3.6.1.jar" individualBasedModel/DiscreteSpatialPhyloSimulator $XML' >> $EXE
 echo 'cd $ORIG' >> $EXE
 chmod a+x $EXE
